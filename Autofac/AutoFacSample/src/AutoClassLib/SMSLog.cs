2b98dce778267394d9b78349092e387b3838e25c
Fu Xu <xufu2022@gmail.comm>
2023-04-30 06:58:53 +0100

    update ch3

---
+++ Autofac/AutoFacSample/src/AutoClassLib/SMSLog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoClassLib
+{
+    public class SMSLog : ILog
+    {
+        private string phoneNumber;
+
+        public SMSLog(string phoneNumber)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public void Write(string message)
+        {
+            Console.WriteLine($"SMS to {phoneNumber} : {message}");
+        }
+    }
+
+    public class Parent
+    {
+        public override string ToString()
+        {
+            return "I am your father";
+        }
+    }
+
+    public class Child
+    {
+        public string Name { get; set; }
+        public Parent Parent { get; set; }
+
+        public void SetParent(Parent parent)
+        {
+            Parent = parent;
+        }
+    }
+}
--- Autofac/AutoFacSample/src/AutoFacSample/Program.cs
+++ Autofac/AutoFacSample/src/AutoFacSample/Program.cs
@@ -1,40 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AutoClassLib;
 using Autofac;
+using Autofac.Core;
+using System.Reflection;
 
 var builder = new ContainerBuilder();
-//builder.RegisterType<EmailLog>()
-//    .As<ILog>();
-//builder.RegisterType<ConsoleLog>()
-//    .As<ILog>()
-//    .As<IConsole>()
-//    .PreserveExistingDefaults();
-//builder.RegisterType<Engine>();
-//builder.RegisterType<Car>();
+//builder.RegisterType<Parent>();
+
+//builder.RegisterType<Child>()
+//    .OnActivated((IActivatedEventArgs<Child> e) =>
+//    {
+//        var p = e.Context.Resolve<Parent>();
+//        e.Instance.SetParent(p);
+//    })
+//    ;
+
+//var container = builder.Build();
+////var parent = container.Resolve<Parent>();
+//var parent = container.Resolve<Child>().Parent;
+//Console.WriteLine(parent);
+
+var assembly = Assembly.GetExecutingAssembly();
+builder.RegisterAssemblyTypes(assembly)
+    .Where(t => t.Name.EndsWith("Log"))
+    .Except<SMSLog>()
+    .Except<ConsoleLog>(c => c.As<ILog>().SingleInstance())
+    .AsSelf();
+//log apart from smslog and consolelog
 
 
-builder.RegisterType<ConsoleLog>().As<ILog>();
-//var log = new ConsoleLog();
-//builder.RegisterInstance(log).As<ILog>();
+builder.RegisterAssemblyTypes(assembly)
+    .Except<SMSLog>()
+    .Where(t => t.Name.EndsWith("Log"))
+    .As(t => t.GetInterfaces()[0]);
 
-//builder.RegisterType<Engine>();
-//builder.RegisterType<Car>()
-//    .UsingConstructor(typeof(Engine));
-//.UsingConstructor(typeof(Engine), typeof(ILog));
+builder.RegisterAssemblyModules(typeof(Program).Assembly);
+
+var container = builder.Build();
+Console.WriteLine(container.Resolve<Child>().Parent);
+//specifies that each type should be registered with the container using its first implemented interface as the service type.
+//This means that when a component requests an instance of an interface that is implemented by one of the registered types, the container will resolve and return an instance of that type.
 
 
-builder.Register((IComponentContext c) =>
-    new Engine(c.Resolve<ILog>(), 123));
-builder.RegisterType<Car>();
-
-builder.RegisterGeneric(typeof(List<>)).As(typeof(IList<>));
-
-IContainer container = builder.Build();
-
-//var log = container.Resolve<ILog>();
-
-var myList = container.Resolve<IList<int>>();
-Console.WriteLine(myList.GetType());
-
-var car = container.Resolve<Car>();
-car.Go();
+public class ParentChildModule : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<Parent>();
+        builder.Register(c => new Child() { Parent = c.Resolve<Parent>() });
+    }
+}

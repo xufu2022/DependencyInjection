5b9acf0279b47cf3b5966cf45d9fb1d4c5d41e08
Fu Xu <xufu202@gmail.com>
2023-05-19 02:13:10 +0100

    AsSelf for Resolve Consolelog directly

--- Autofac/languages/sample/src/AutofacSteps/AutoFacTest/LogTest.cs
+++ Autofac/languages/sample/src/AutofacSteps/AutoFacTest/LogTest.cs
@@ -1,13 +1,32 @@
-﻿namespace AutoFacTest
+﻿
+
+using AutofacSamples;
+
+namespace AutoFacTest
 {
     public class LogTest
     {
         [Fact]
         public void Sample1Test()
         {
-            var log = new ConsoleLog();
-            var engine = new Engine(log);
-            var car = new Car(engine, log);
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleLog>().As<ILog>().AsSelf();
+            builder.RegisterType<Engine>();
+            builder.RegisterType<Car>();
+            var container=builder.Build();
+            //var log = new ConsoleLog();
+            //var engine = new Engine(log);
+            //var car = new Car(engine, log);
+
+            //if want to resolve consolelog, will failed
+            //the above  builder.RegisterType<ConsoleLog>().As<ILog>();
+            //without AsSelf, cannot resolve directly
+            var log =container.Resolve<ConsoleLog>();
+            Assert.NotNull(log);
+
+
+            var car = container.Resolve<Car>();
+            Assert.NotNull(car);
             car.Go();
 
         }

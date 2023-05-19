4debe0a42a11d2cb31bec388c25fba674f6f2303
Fu Xu <xufu202@gmail.com>
2023-05-19 01:59:42 +0100

    without di

---
+++ Autofac/languages/sample/src/AutofacSteps/AutoFacTest/LogTest.cs
@@ -0,0 +1,16 @@
+﻿namespace AutoFacTest
+{
+    public class LogTest
+    {
+        [Fact]
+        public void Sample1Test()
+        {
+            var log = new ConsoleLog();
+            var engine = new Engine(log);
+            var car = new Car(engine, log);
+            car.Go();
+
+        }
+    }
+
+}
